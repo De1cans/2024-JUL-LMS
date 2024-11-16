7fb56fee08b712a0a3db1325566954895d28ca4b
De1cans <15901@coderacademy.edu.au>
2024-11-16 11:59:22 +1100

    Implimented create operation on student entity

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,4 +1,6 @@
-from Flask import Blueprint
+from Flask import Blueprint, request
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 
 from init import db
 from models.student import Student, student_schema, students_schema
@@ -21,3 +23,24 @@ def get_student(student_id):
         return data
     else:
         return {"message": f"Student with id {student_id} does not exist"}, 404
+
+@student_bp.route("/", methods=["POST"])
+def create_student():
+    try:
+        body_data = request.get_json()
+        new_student = Student(
+            name=body_data.get("name"),
+            email=body_data.get("email"),
+            address=body_data.get("address")
+        )
+        db.session.add(new_student)
+        db.session.commit()
+        return student_schema.dump(new_student), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not null violation
+            return {"message": f"The field {err.orig.diag.column_name} is required"}, 409
+        
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            # unique constriant violation
+            return {"message": "Email address already in use"}, 409
