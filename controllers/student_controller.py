cc8d39d684e81f5b3e5308177133e1162d7e5bb4
De1cans <15901@coderacademy.edu.au>
2024-11-16 12:16:42 +1100

    added functions

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -44,3 +44,28 @@ def create_student():
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             # unique constriant violation
             return {"message": "Email address already in use"}, 409
+
+@student_bp.route("/<int:student_id>", methods=["DELETE"])
+def delete_student(student_id):
+    stmt = db.select(Student).filter_by(id=student_id)
+    student = db.session.scalar(stmt)
+    if student:
+        db.session.delete(student)
+        db.session.commit()
+        return {"message": f"Student with id {student_id} has been deleted successfully"}
+    else: 
+        return {"message": f"Student with id {student_id} does not exist"}, 404
+
+@student_bp.route("/<int:student_id", methods=["PUT", "PATCH"])
+def update_student(student_id):
+    stmt = db.select(Student).filter_by(id=student_id)
+    student = db.session.scalar(stmt)
+    body_data = request.get_json()
+    if student:
+        student_name = body_data.get("name") or student.name
+        student_email = body_data.get("email") or student.email
+        student_address = body_data.get("address") or student.address
+        db.session.commit()
+        return student_schema.dump(student)
+    else:
+        return {"message": f"Student with id {student_id} does not exist"}, 404
