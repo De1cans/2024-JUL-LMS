d676c85424f9fb6db8a2990da27fd8ebdd3fe71b
De1cans <15901@coderacademy.edu.au>
2024-11-25 19:53:11 +1100

    science

--- controllers/enrolment_controller.py
+++ controllers/enrolment_controller.py
@@ -1,13 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from init import db
 from models.enrolment import Enrolment, enrolments_schema, enrolment_schema
 
 enrolments_bp = Blueprint("enrolments", __name__, url_prefix="/enrolments")
 
+# Read all
 @enrolments_bp.route("/")
 def get_enrolments():
-    stmt = db.select(Enrolment)
+    student_id = request.args.get("student_id")
+    if student_id:
+        stmt = db.select(Enrolment).filter_by(student_id=student_id)
+    else:
+        stmt = db.select(Enrolment)
     enrolments_list = db.session.scalars(stmt)
     return enrolments_schema.dump(enrolments_list)
 
@@ -15,10 +20,8 @@ def get_enrolments():
 # Read one
 @enrolments_bp.route("/<int:enrolment_id>")
 def get_enrolment(enrolment_id):
-    if: 
-        stmt = db.select(Enrolment).filter_by(student_id=student_id)
-    else:
-        enrolment = db.session.scalar(stmt)
+    stmt = db.select(Enrolment).filter_by(id=enrolment_id)
+    enrolment = db.session.scalar(stmt)
     if enrolment:
         return enrolment_schema.dump(enrolment)
     else:
